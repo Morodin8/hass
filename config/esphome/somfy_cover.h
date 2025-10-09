5ae16794992f0abcb81560607b778b017d4fddbc
Gray <grahamseymore@hotmail.com>
2025-10-09 15:49:35 +0100

    Refactor Somfy cover implementation and improve setup

--- config/esphome/somfy_cover.h
+++ config/esphome/somfy_cover.h
@@ -1,25 +1,53 @@
-#include "esphome.h"
+#pragma once
+
+#include "esphome/components/cover/cover.h"
+#include "esphome/core/component.h"
 #include <ELECHOUSE_CC1101_SRC_DRV.h>
 #include <NVSRollingCodeStorage.h>
 #include <SomfyRemote.h>
 
-#define EMITTER_GPIO 2
-
 #define CC1101_FREQUENCY 433.42
 
-#define COVER_OPEN 1.0f
-#define COVER_CLOSED 0.0f
+namespace esphome {
+namespace somfy {
 
-class SomfyESPCover : public Cover {
-private:
-  SomfyRemote *remote;
-  NVSRollingCodeStorage *storage;
+using namespace esphome::cover;
+
+static const char *const TAG = "somfy.cover";
+
+class SomfyCover : public Cover, public Component {
+protected:
+  SomfyRemote *remote_;
+  NVSRollingCodeStorage *storage_;
+  const char *storage_namespace_;
+  const char *storage_key_;
+  InternalGPIOPin *emitter_pin_;
+  uint32_t remote_address_;
+  int repeat_;
 
 public:
-  SomfyESPCover(const char *name, const char *key, uint32_t remoteCode)
-      : Cover() {
-    storage = new NVSRollingCodeStorage(name, key);
-    remote = new SomfyRemote(EMITTER_GPIO, remoteCode, storage);
+  void setup() override {
+    this->emitter_pin_->pin_mode(gpio::FLAG_OUTPUT);
+    this->emitter_pin_->digital_write(false);
+
+    ELECHOUSE_cc1101.setSpiPin(18, 19, 23, 21);  // SCK, MISO, MOSI, CS
+    ELECHOUSE_cc1101.Init();
+    if (ELECHOUSE_cc1101.getCC1101()) {
+      ESP_LOGI(TAG, "CC1101 connection OK");
+    } else {
+      ESP_LOGE(TAG, "CC1101 connection ERROR (check SPI wiring)");
+    }
+    ELECHOUSE_cc1101.setMHZ(CC1101_FREQUENCY);
+    // Ensure OOK/ASK modulation and reasonable power level for Somfy
+    // Modulation: 2 = ASK/OOK in ELECHOUSE driver
+    ELECHOUSE_cc1101.setModulation(2);
+    // Set TX power (0..10)
+    ELECHOUSE_cc1101.setPA(10);
+    ESP_LOGI(TAG, "CC1101 configured: %.2f MHz, OOK, PA=10", (double)CC1101_FREQUENCY);
+
+    storage_ = new NVSRollingCodeStorage(storage_namespace_, storage_key_);
+    remote_ = new SomfyRemote(emitter_pin_->get_pin(), remote_address_, storage_);
+    remote_->setup();
   }
 
   CoverTraits get_traits() override {
@@ -27,12 +55,13 @@ public:
     traits.set_is_assumed_state(true);
     traits.set_supports_position(false);
     traits.set_supports_tilt(false);
+    traits.set_supports_stop(true);
     return traits;
   }
 
   void sendCC1101Command(Command command) {
     ELECHOUSE_cc1101.SetTx();
-    remote->sendCommand(command, 1);
+    remote_->sendCommand(command, this->repeat_);
     ELECHOUSE_cc1101.setSidle();
   }
 
@@ -41,13 +70,13 @@ public:
       float pos = *call.get_position();
 
       if (pos == COVER_OPEN) {
-        ESP_LOGI("somfy", "OPEN");
+        ESP_LOGI(TAG, "OPEN");
         sendCC1101Command(Command::Up);
       } else if (pos == COVER_CLOSED) {
-        ESP_LOGI("somfy", "CLOSE");
+        ESP_LOGI(TAG, "CLOSE");
         sendCC1101Command(Command::Down);
       } else {
-        ESP_LOGI("somfy", "WAT");
+        ESP_LOGI(TAG, "WAT");
       }
 
       this->position = pos;
@@ -55,32 +84,27 @@ public:
     }
 
     if (call.get_stop()) {
-      ESP_LOGI("somfy", "STOP");
+      ESP_LOGI(TAG, "STOP");
       sendCC1101Command(Command::My);
     }
   }
 
   void program() {
-    ESP_LOGI("somfy", "PROG");
+    ESP_LOGI(TAG, "PROG");
     sendCC1101Command(Command::Prog);
   }
+
+  void set_pin(InternalGPIOPin *pin) { this->emitter_pin_ = pin; }
+
+  void set_remote_address(uint32_t remote_address) { this->remote_address_ = remote_address; }
+  void set_storage_namespace(const char *storage_namespace) {
+    this->storage_namespace_ = storage_namespace;
+  }
+  void set_storage_key(const char *storage_key) {
+    this->storage_key_ = storage_key;
+  }
+  void set_repeat(int repeat) { this->repeat_ = repeat; }
 };
 
-class SomfyESPRemote : public Component {
-public:
-  std::vector<esphome::cover::Cover *> covers;
-
-  void setup() override {
-    // need to set GPIO PIN 4 as OUTPUT, otherwise no commands will be sent
-    pinMode(EMITTER_GPIO, OUTPUT);
-    digitalWrite(EMITTER_GPIO, LOW);
-
-    ELECHOUSE_cc1101.Init();
-    ELECHOUSE_cc1101.setMHZ(CC1101_FREQUENCY);
-  }
-
-  void add_cover(const char *name, const char *key, uint32_t remoteCode) {
-    auto cover = new SomfyESPCover(name, key, remoteCode);
-    covers.push_back(cover);
-  }
-};
+} // namespace somfy
+} // namespace esphome
